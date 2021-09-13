b344d6e437cd8efc1d9c38a8e7706846abc07ed7
Maurits Lamers <maurits@weidestraat.nl>
2021-09-13 13:36:00 +0200

    update package.json for proper use by webpack

---
+++ sproutcore_global.js
@@ -0,0 +1,25 @@
+import { SC as core, GLOBAL } from './core/core.js';
+import * as event from './event/event.js';
+import * as responder from './responder/responder.js';
+import * as statechart from './statechart/statechart.js';
+import * as view from './view/view.js';
+import * as datastore from './datastore/datastore.js';
+import * as desktop from './desktop/desktop.js';
+import * as ajax from './ajax/ajax.js';
+
+export const SproutCore = core;
+
+// this is done as the SC.mixin tries hasOwnProperty which is not present on 
+// the imported name spaces
+[event, responder, statechart, view, datastore, desktop, ajax].forEach(fw => {
+  Object.keys(fw).forEach(k => {
+    SproutCore[k] = fw[k];
+  });
+});
+
+export const SC = SproutCore;
+
+GLOBAL.SC = GLOBAL.Sproutcore = SC.mixin(GLOBAL.SC, SproutCore);
+// backwards compat
+GLOBAL.YES = true;
+GLOBAL.NO = false;
