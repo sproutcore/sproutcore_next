a41d3b3f8dc8e3762fc7957ef2e98f94923f262b
Maurits Lamers <mlamers@turnitin.com>
2023-03-03 19:13:11 +0100

    fix missing declaration

--- core/system/builder.js
+++ core/system/builder.js
@@ -9,6 +9,8 @@ import { A, beget, mixin, typeOf } from "./base.js";
 import { T_ARRAY } from "./constants.js";
 import "../ext/string.js";
 import { Enumerable } from '../mixins/enumerable.js';
+import { SC } from '../core.js';
+
 
 /** @class
 
@@ -16,77 +18,77 @@ import { Enumerable } from '../mixins/enumerable.js';
   those provided by CoreQuery or jQuery.  Usually you will not create a new
   builder yourself, but you will often use instances of the Builder object to
   configure parts of the UI such as menus and views.
-  
+
   # Anatomy of a Builder
-  
-  You can create a new Builder much like you would any other class in 
+
+  You can create a new Builder much like you would any other class in
   SproutCore.  For example, you could create a new CoreQuery-type object with
   the following:
-  
+
       $ = Builder.create({
         // methods you can call go here.
       });
-  
-  Unlike most classes in SproutCore, Builder objects are actually functions 
-  that you can call to create new instances.  In the example above, to use 
+
+  Unlike most classes in SproutCore, Builder objects are actually functions
+  that you can call to create new instances.  In the example above, to use
   the builder, you must call it like a function:
-  
+
       buildit = $();
-  
+
   If you define an init() method on a builder, it will be invoked wheneve the
   builder is called as a function, including any passed params.  Your init()
   method MUST return this, unlike regular SC objects.  i.e.
-  
+
       $ = Builder.create({
-        init: function(args) { 
+        init: function(args) {
           this.args = A(args);
           return this;
         }
       });
-      
+
       buildit = $('a', 'b');
       buildit.args => ['a','b']
-  
+
   In addition to defining a function like this, all builder objects also have
   an 'fn' property that contains a hash of all of the helper methods defined
-  on the builder function.  Once a builder has been created, you can add 
+  on the builder function.  Once a builder has been created, you can add
   addition "plugins" for the builder by simply adding new methods to the
   fn property.
-  
+
   # Writing Builder Functions
-  
+
   All builders share a few things in common:
-  
+
    * when a new builder is created, it's init() method will be called.  The default version of this method simply copies the passed parameters into the builder as content, but you can override this with anything you want.
    * the content the builder works on is stored as indexed properties (i.e. 0,1,2,3, like an array).  The builder should also have a length property if you want it treated like an array.
    *- Builders also maintain a stack of previous builder instances which you can pop off at any time.
-  
+
   To get content back out of a builder once you are ready with it, you can
-  call the method done().  This will return an array or a single object, if 
+  call the method done().  This will return an array or a single object, if
   the builder only works on a single item.
-  
+
   You should write your methods using the getEach() iterator to work on your
   member objects.  All builders implement Enumerable in the fn() method.
 
       CoreQuery = Builder.create({
         ...
       }) ;
-      
+
       CoreQuery = new Builder(properties) {
         ...
       } ;
-      
+
       CoreQuery2 = CoreQuery.extend() {
       }
-  
+
   @constructor
 */
 export const Builder = function (props) { return Builder.create(props); };
 
-/** 
-  Create a new builder object, applying the passed properties to the 
+/**
+  Create a new builder object, applying the passed properties to the
   builder's fn property hash.
-  
+
   @param {Object} props
   @returns {Builder}
 */
@@ -100,9 +102,9 @@ Builder.create = function create(props) {
   var construct = function () {
     var ret = beget(fn); // NOTE: using closure here...
 
-    // the defaultClass is usually this for this constructor. 
+    // the defaultClass is usually this for this constructor.
     // e.g. View.build() -> this = View
-    ret.defaultClass = this || SC; // fallback 
+    ret.defaultClass = this || SC; // fallback
     ret.constructor = construct;
 
     // now init the builder object.
@@ -127,13 +129,13 @@ Builder.mixin = function () {
 /** This is the default set of helper methods defined for new builders. */
 Builder.fn = {
 
-  /** 
+  /**
     Default init method for builders.  This method accepts either a single
-    content object or an array of content objects and copies them onto the 
-    receiver.  You can override this to provide any kind of init behavior 
-    that you want.  Any parameters passed to the builder method will be 
+    content object or an array of content objects and copies them onto the
+    receiver.  You can override this to provide any kind of init behavior
+    that you want.  Any parameters passed to the builder method will be
     forwarded to your init method.
-    
+
     @returns {Builder} receiver
   */
   init: function (content) {
@@ -154,10 +156,10 @@ Builder.fn = {
   /** Return the number of elements in the matched set. */
   size: function () { return this.length; },
 
-  /** 
+  /**
     Take an array of elements and push it onto the stack (making it the
     new matched set.)  The receiver will be saved so it can be popped later.
-    
+
     @param {Object|Array} content
     @returns {Builder} new instance
   */
